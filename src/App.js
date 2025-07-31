c4c1b106b97b763cdd43b3d971ef24ae41a0c312
Udhaya-2015 <2015udhayaautostore@gmail.com>
2025-07-31 19:32:13 +0530

    V 0.1
    mend

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/navbar";
 import Home from "./pages/Home";
 import SearchPage from "./pages/SearchPage";
 import AddProduct from "./pages/AddProduct";
